df167936c53763edbbe745e9847e9c68c68c1e80
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-05-16 16:41:22 +0300

    Fixed knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,51 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-module.exports = {
-  development: {
-    client: "mysql",
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_DATABASE,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  staging: {
-    client: "mysql",
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_DATABASE,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "mysql",
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_DATABASE,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
+module.exports = async () => {
+  const { default: configurations } = await import("./app/Config/Database.js");
+  return configurations;
 };
