7d1b16eb5a20e23771aed609f2cae4d9f7bec716
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-08-20 22:36:15 +0300

    Fixed initial conditions

--- app/Config/Database.js
+++ app/Config/Database.js
@@ -5,5 +5,8 @@ export default {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    searchPath: [process.env.DB_USER, "public"],
+    filename: `${process.env.DB_DATABASE}.sqlite`,
   },
 };
--- app/init.js
+++ app/init.js
@@ -1,7 +1,1 @@
-const health = (req, res) => {
-  return res.json({});
-};
-
-export default async ({ app }) => {
-  app.get("/api/health", health);
-};
+export default async ({ app }) => {};
