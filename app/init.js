58dfb975f698605b90ffac9d88c571cf988d5e3c
Ozgur Adem ISIKLI <ozgur@shape.dk>
2022-12-24 16:50:25 +0100

    Added typescript changes

--- app/init.js
+++
@@ -1,5 +0,0 @@
-const onBeforeInit = async ({ app }) => {};
-
-const onAfterInit = async ({ app }) => {};
-
-export { onBeforeInit, onAfterInit };
--- index.js
+++
@@ -1,6 +0,0 @@
-import path from "path";
-import { Server } from "axe-api";
-
-const appFolder = path.join(path.resolve(), "app");
-const server = new Server(appFolder);
-server.listen();
