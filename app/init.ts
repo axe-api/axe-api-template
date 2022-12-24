0ff58e66a5ce2b44f76f6686a841ddf4103b6837
Ozgur Adem ISIKLI <ozgur@shape.dk>
2022-12-24 20:40:31 +0100

    Fixed sqlite bugs

--- app/init.ts
+++ app/init.ts
@@ -1,6 +1,15 @@
 import { Express } from "express";
 
-const onBeforeInit = async (app: Express) => {};
+const onBeforeInit = async (app: Express) => {
+  app.get("/", (req, res) => {
+    res.json({
+      name: "AXE API",
+      description: "The next generation Rest API framework.",
+      documentation:
+        "https://axe-api.github.io/getting-started/crud/index.html",
+    });
+  });
+};
 
 const onAfterInit = async (app: Express) => {};
 
