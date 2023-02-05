cfc4f7516d8327dd8a6bac7919679925c2be57c4
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-02-05 19:29:28 +0100

    Fixed

---
+++ app/config.ts
@@ -0,0 +1,28 @@
+import { LogLevels, IApplicationConfig } from "axe-api";
+
+const config: IApplicationConfig = {
+  prefix: "api",
+  env: process.env.NODE_ENV || "production",
+  port: process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000,
+  logLevel: LogLevels.INFO,
+  database: {
+    client: process.env.DB_CLIENT || "mysql",
+    connection: {
+      host: process.env.DB_HOST || "localhost",
+      user: process.env.DB_USER || "user",
+      password: process.env.DB_PASSWORD || "password",
+      database: process.env.DB_DATABASE || "database",
+      filename: `${process.env.DB_DATABASE}.sqlite`,
+    },
+    searchPath: [process.env.DB_USER || "user", "public"],
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+export default config;
