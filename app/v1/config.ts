0be39e5add8cbcc0f6e1dae15ce55629e7d358d0
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-02-05 19:29:28 +0100

    Fixed

---
+++ app/v1/config.ts
@@ -0,0 +1,10 @@
+import { IVersionConfig } from "axe-api";
+
+const config: IVersionConfig = {
+  transaction: [],
+  serializers: [],
+  supportedLanguages: ["en"],
+  defaultLanguage: "en",
+};
+
+export default config;
